fd8a23b050f477daccee7bb8f70eba8bd6a6cb8a
Umar Ali <umarali@Umars-MacBook-Pro.local>
2025-03-11 12:56:26 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "ibm_schematics_action" "schematics_action" {
+  name = "test"
+  description = "test"
+  location = "eu-de"
+  resource_group = "default"
+}
---
+++ version.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.76.1"
+    }
+  }
+}
+
+provider "ibm" {
+  # Configuration options
+}
